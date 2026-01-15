8cbc74aa83800c755c980ad285b356b2bde0d44d
Romain Thoyer <51092742+rthoyer@users.noreply.github.com>
2026-01-15 17:14:24 +0100

    keep 10 topics limit

--- agora_topic_modeling/code/topic_modeling.py
+++ agora_topic_modeling/code/topic_modeling.py
@@ -28,7 +28,8 @@ def get_lightweight_bertopic_model(X: pd.Series)-> tuple[BERTopic, pd.DataFrame]
         TfidfVectorizer(),
         TruncatedSVD(100)
     )
-    topic_model = BERTopic(embedding_model=pipe, vectorizer_model=vectorizer_model, language="french", verbose=True)
+    nr_topics = 10
+    topic_model = BERTopic(embedding_model=pipe, vectorizer_model=vectorizer_model, nr_topics=nr_topics, language="french", verbose=True)
     print("Fit Transform")
     topics, probs = topic_model.fit_transform(X)
     print("Returns")
@@ -46,7 +47,7 @@ def get_custom_bertopic_model(X: pd.Series, nr_topics: int=10, min_topic_size: i
     if not sub_topics:
         print("Topic model")
         #topic_model = BERTopic(embedding_model=embedding_model, vectorizer_model=vectorizer_model, nr_topics=nr_topics, min_topic_size=min_topic_size, language="french", verbose=True)
-        topic_model = BERTopic(vectorizer_model=vectorizer_model, language="french", verbose=True)
+        topic_model = BERTopic(vectorizer_model=vectorizer_model, nr_topics=nr_topics, language="french", verbose=True)
     else:
         print("SubTopic model")
         n_docs = 2 if round(X.size * 0.02) < 2 else round(X.size * 0.02)
